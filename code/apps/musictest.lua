7cf148eee8028889e6c62a37e38f8f26d728dd53
chenxuuu <lolicon@papapoi.com>
2018-11-25 23:20:44 +0800

    更正语音bug

--- code/apps/musictest.lua
+++ code/apps/musictest.lua
@@ -10,6 +10,7 @@ local function exit()
     uiWin.remove(id)
 end
 
+local playlock = false
 function load()
     id = uiWin.add({
         onUpdate = function ()
@@ -17,17 +18,22 @@ function load()
             tools.puttext("声音测试：", 0,0)
             tools.puttext("黄键播放mp3", 0,17)
             tools.puttext("红键播放tts", 0,33)
-
             disp.update()
         end,
         onKey = function (key,value)
             if value == 0 and key == "green" then
                 exit()
                 return
-            elseif value == 0 and key == "red" then
-                audio.play(TTS,"TTS","语音测试",nvm.get("vol"))
-            elseif value == 0 and key == "yellow" then
-                tools.music("poweron.mp3")
+            elseif value == 0 and key == "red" and not playlock then
+                playlock = true
+                audio.play(TTS,"TTS","语音测试",nvm.get("vol"),function ()
+                    playlock = false
+                end)
+            elseif value == 0 and key == "yellow" and not playlock then
+                playlock = true
+                tools.music("poweron.mp3",false,function ()
+                    playlock = false
+                end)
             end
             uiWin.update()
         end
